b7f90c37362f317dceb81f75e30c908b11b758ec
allenalvin333 <allenalvin333@gmail.com>
2021-07-25 19:00:15 +0530

    programs

--- July21/Starters7/5.py
+++ July21/Starters7/5.py
@@ -1,2 +1,7 @@
 # https://www.codechef.com/START7C/problems/MAXSWT
 
+for T in range(int(input())):
+    N,D=map(int,input().split())
+    n=list(map(int,input().split()))
+    s=list(map(int,input().split()))
+    
