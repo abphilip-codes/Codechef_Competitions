875c059cd5eab57288f33fa46d2ba220a6e7f79a
allenalvin333 <allenalvin333@gmail.com>
2021-07-25 16:46:39 +0530

    programs

--- July21/Starters7/4.py
+++ July21/Starters7/4.py
@@ -3,8 +3,5 @@
 import math
 for T in range(int(input())):
     a,b=map(int,input().split())
-    maximum=-32700
-    for x in range(min(a,b),max(a,b)+1):
-        if(math.ceil((b-x)/2)+math.ceil((x-a)/2)>maximum): 
-            maximum=math.ceil((b-x)/2)+math.ceil((x-a)/2)
-    print(maximum)
+    if(a==b):print(0)
+    else:print(math.ceil((b-min(a,b)-1)/2)+math.ceil((min(a,b)+1-a)/2))
