0a157935f98c8e26b0cc97bbe2eef28896026189
allenalvin333 <allenalvin333@gmail.com>
2021-08-08 09:31:17 +0530

    programs

--- Aug21/Long/4.py
+++ Aug21/Long/4.py
@@ -1,2 +1,7 @@
 # https://www.codechef.com/AUG21C/problems/SPCTRIPS
 
+for T in range(int(input())):
+    n,c=int(input()),0
+    for x in range(1,n+1):
+        for y in range(x,n+1,x): c+=((n-(x+y))//y+1)
+    print(c)
