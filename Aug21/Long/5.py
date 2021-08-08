1ce2245bd07122b672604fe52c196f899f3401f2
allenalvin333 <allenalvin333@gmail.com>
2021-08-08 10:05:12 +0530

    programs

--- Aug21/Long/5.py
+++ Aug21/Long/5.py
@@ -1,2 +1,11 @@
 # https://www.codechef.com/AUG21C/problems/ARRFILL
 
+for T in range(int(input())):
+    n,m=map(int,input().split())
+    x=y=[]
+    A=[0]*n
+    for i in range(m):
+        X,Y=map(int,input().split())
+        x.append(X)
+        y.append(Y)
+    
