36aa9e6e741ac616840bde368ab0cc36b368b26d
allenalvin333 <allenalvin333@gmail.com>
2021-08-15 20:03:54 +0530

    programs

---
+++ Aug21/Starters8/1.py
@@ -0,0 +1,12 @@
+#
+
+for T in range(int(input())):
+    n=list(map(int,input().split()))
+    a=b=0
+    for i in range(len(n)):
+        if(n[i]==1):
+            if(i%2==0): a+=1
+            else: b+=1 
+    if(a>b): print(1) 
+    elif(b>a): print(2)
+    else: print(0)
