105f756aea98a9e35c9fc3e2275e94c7c002f4db
allenalvin333 <allenalvin333@gmail.com>
2021-08-15 21:51:46 +0530

    programs

--- Aug21/Starters8/3.py
+++ Aug21/Starters8/3.py
@@ -4,4 +4,16 @@ for T in range(int(input())):
     ax,ay=map(int,input().split())
     bx,by=map(int,input().split())
     cx,cy=map(int,input().split())
-    print("YES") if((ax==bx or ay==by) and (bx==cx or by==cy)) else print("NO")
+    turn=0
+    if(ax==bx or ay==by): turn+=0
+    else: turn+=1
+    if(cx==bx or cy==by): 
+        if(cx==bx and ax==bx): 
+            if(bx not in range(min(ax,cx),max(ax,cx)+1)): turn=2
+            turn+=0
+        elif(cy==by and ay==by): 
+            if(by not in range(min(ay,cy),max(ay,cy)+1)): turn=2
+            turn+=0
+        else: turn+=1
+    else: turn+=2
+    print("YES") if(turn<2) else print("NO")
