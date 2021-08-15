520dc41ff2cdd1cc2d85f7758d20234b8fee5467
allenalvin333 <allenalvin333@gmail.com>
2021-08-15 20:27:30 +0530

    programs

--- Aug21/Starters8/2.py
+++ Aug21/Starters8/2.py
@@ -1,2 +1,15 @@
 # https://www.codechef.com/START8C/problems/JAVELIN
 
+for T in range(int(input())):
+    (n,m,x)=map(int,input().split())
+    fin=sel=[]
+    a=list(map(int,input().split()))
+    for i in range(n):
+        if(a[i]>=m): fin.append(i+1)
+    if(len(fin)<x):
+        sel=sorted(a)[0-x:]
+        fin=[]
+        for z in sel:
+            fin.append(a.index(z)+1)
+    print(len(fin),end=" ")
+    print(*sorted(fin))
